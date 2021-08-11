bccdbcc15c37e92da20fa974e4aaf7842ba04ea3
Emmanuel Granara <emanuelgranara@hotmail.com>
2021-08-11 11:22:36 -0300

    back finished

--- api/index.js
+++ api/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const multer = require('multer');
+
 const authRoute = require('./routes/auth');
 const userRoute = require('./routes/users');
 const postRoute = require('./routes/posts');
-
+const categoryRoute = require('./routes/categories');
 
 dotenv.config();
 app.use(express.json()); // to can send json
@@ -19,10 +21,26 @@ mongoose
   .then(console.log('Connected to mongoDB'))
   .catch((err) => console.log(err));
 
+// ceate storage for images
+const storage = multer.diskStorage({
+  destination: (req, file, callb) => {
+    callb(null, 'images'); // null for errors
+  },
+  filename: (req, file, callb) => {
+    callb(null, req.body.name);
+  },
+});
+
+// method for upload
+const upload = multer({ storage: storage });
+app.post('/api/upload', upload.single('file'), (req, res) => {
+  res.status(200).json('File has been uploaded');
+});
+
 app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
 app.use('/api/posts', postRoute);
-
+app.use('/api/categories', categoryRoute);
 
 app.listen('5000', () => {
   console.log('Backend is running');
--- api/routes/posts.js
+++ api/routes/posts.js
@@ -14,18 +14,75 @@ router.post('/', async (req, res) => {
 });
 
 // Update Post
-router.delete('/:id', async (req, res) => {
-  
+router.put('/:id', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.username === req.body.username) {
+      try {
+        const updatedPost = await Post.findByIdAndUpdate(
+          req.params.id,
+          { $set: req.body },
+          { new: true }
+        );
+        res.status(200).json(updatedPost);
+      } catch (error) {
+        res.status(500).json(error);
+      }
+    } else {
+      res.status(401).json('You can update only your post!');
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 // Delete Post
+router.delete('/:id', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.username === req.body.username) {
+      try {
+        await post.delete();
+        res.status(200).json('Post has been deleted...');
+      } catch (error) {
+        res.status(500).json(error);
+      }
+    } else {
+      res.status(401).json('You can delete only your post!');
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 // Get Post
 router.get('/:id', async (req, res) => {
   try {
-    const user = await User.findById(req.params.id);
-    const { password, ...others } = user._doc;
-    res.status(200).json(others);
+    const post = await Post.findById(req.params.id);
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(500).json(500);
+  }
+});
+
+// Get all posts
+router.get('/', async (req, res) => {
+  const username = req.query.user;
+  const catName = req.query.cat;
+
+  try {
+    let posts;
+    if (username) {
+      posts = await Post.find({ username });
+    } else if (catName) {
+      posts = await Post.find({
+        categories: { $in: [catName] },
+      });
+    } else {
+      // all posts
+      posts = await Post.find();
+    }
+    res.status(200).json(posts);
   } catch (error) {
     res.status(500).json(500);
   }
