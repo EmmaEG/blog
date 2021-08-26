8200e48642a1f665cf88026c160ba6398864ba09
Emmanuel Granara <emanuelgranara@hotmail.com>
2021-08-26 12:20:35 -0300

    single post and update post

--- api/index.js
+++ api/index.js
@@ -8,9 +8,11 @@ const authRoute = require('./routes/auth');
 const userRoute = require('./routes/users');
 const postRoute = require('./routes/posts');
 const categoryRoute = require('./routes/categories');
+const path = require('path');
 
 dotenv.config();
 app.use(express.json()); // to can send json
+app.use("/images", express.static(path.join(__dirname, "/images")))
 
 mongoose
   .connect(process.env.MONGO_URL, {
